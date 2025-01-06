dccad43d7a09ee3eb480b3a03ad665ffa52a0723
Michael Lacanilao <michael.v.lacanilao@gmail.com>
2025-01-06 14:07:56 -0500

    Fix negative tag traits

--- Patches/CardPatch.cs
+++ Patches/CardPatch.cs
@@ -4,14 +4,16 @@ namespace NoNegativeFoodTraits.Patches
     {
         public static void CreatePostfix(Thing __instance)
         {
+            if (__instance.IsFood == false)
+            {
+                return;
+            }
+            
             foreach (Element element in __instance.elements.dict.Values)
             {   
-                if (element.IsFoodTrait)
+                if (element.Value <= 0)
                 {
-                    if (element.Value <= 0)
-                    {
-                        __instance.elements.SetTo(id: element.id, v: 0);
-                    }
+                    __instance.elements.SetTo(id: element.id, v: 0);
                 }
             }
         }
