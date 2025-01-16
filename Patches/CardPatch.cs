168bc72a9a5685c416ebfef61d2f2ab356e78d0e
Michael Lacanilao <michael.v.lacanilao@gmail.com>
2025-01-16 18:45:09 -0500

    Fix collection bug

--- Patches/CardPatch.cs
+++ Patches/CardPatch.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace NoNegativeFoodTraits.Patches
 {
     public static class CardPatch
@@ -9,12 +11,18 @@ namespace NoNegativeFoodTraits.Patches
                 return;
             }
             
+            List<Element> negativeElement = new List<Element>();
             foreach (Element element in __instance.elements.dict.Values)
-            {   
+            {
                 if (element.Value <= 0)
                 {
-                    __instance.elements.SetTo(id: element.id, v: 0);
+                    negativeElement.Add(element);
                 }
+            }
+            
+            foreach (Element element in negativeElement)
+            {
+                __instance.elements.SetTo(id: element.id, v: 0);
             }
         }
     }
